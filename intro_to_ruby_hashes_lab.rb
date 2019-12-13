b4937425d01e0087e870365c3cac3a605f73e1b6
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-13 19:39:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,6 +26,7 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   
   testhash={key:value}
+  return testhash
 end
 
 def read_from_hash(hash, key)
