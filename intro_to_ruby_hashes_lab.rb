5e64833f3e4f199155eb863b156ca36bbce6b81c
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-13 19:37:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,8 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  
+  testhash = {name:"Grace Hopper"}
+  return testhash
 end
 
 def id_generator
